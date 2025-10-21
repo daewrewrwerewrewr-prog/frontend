85c82718dd13e11518a7c25f8e290cdf5a131bed
daewrewrwerewrewr-prog <daewrewrwerewrewr@gmail.com>
2025-10-21 18:32:46 +0300

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -94,7 +94,6 @@ export default async function handler(req, res) {
             },
           },
         ],
-        test_event_code: 'TEST49604',
       };
       // Meta ortam değişkenleri
       const metaToken = process.env.META_CONVERSIONS_TOKEN;
